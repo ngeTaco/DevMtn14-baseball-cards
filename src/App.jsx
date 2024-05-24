d6d21ebf58b80eebb449b6d37cb61ad72a9e7e68
ngeTaco <thealdo.531@gmail.com>
2024-05-23 21:22:42 -0600

    step 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import playerData from './playerData.js';
 function BaseballCard(props) {
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{props.name}</h2>
+      <img src={props.imgUrl} alt={props.name}/>
     </div>
   );
 }
 
 function App() {
   const cards = playerData.map(player => {
-    return <BaseballCard />
+    return <BaseballCard 
+    name={player.name}
+    team={player.team}
+    position={player.position}
+    stats={player.stats}
+    imgUrl={player.imgUrl}
+    key={player.cardId}
+    />
   })
   return <>{cards}</>;
 }
